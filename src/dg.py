c60220cb0950895a6c2d05438efa4b479b5fd133
Marlon <marlonvcendron@gmail.com>
2024-09-19 10:57:42 -0300

    better clusterconnect

--- src/dg.py
+++ src/dg.py
@@ -6,6 +6,7 @@ import models.cells.bc
 import models.cells.mc
 import models.cells.hipp
 from utils.connect import clusterConnect
+from utils.debug import printConnections
 
 N_clusters = 20
 N_gcs = 2000  # Número de granule cells
@@ -18,23 +19,31 @@ basket_cells = nest.Create("bc", N_bcs)
 mossy_cells = nest.Create("mc", N_mcs)
 hipp_cells = nest.Create("hipp", N_hipps)
 
+gcs_per_cluster = N_gcs // N_clusters
+mcs_per_cluster = N_mcs // N_clusters
+hipps_per_cluster = N_hipps // N_clusters
+
 # Granule cells connections
-clusterConnect(granule_cells, basket_cells)
-# nest.Connect(granule_cells, basket_cells)
-# nest.Connect(granule_cells, mossy_cells)
-# nest.Connect(granule_cells, hipp_cells)
+for i in range(N_clusters):
+  clusterConnect(granule_cells, basket_cells, N_clusters, i)
+  clusterConnect(granule_cells, mossy_cells, N_clusters, i)
+  clusterConnect(granule_cells, hipp_cells, N_clusters, i)
 
 # Basket cells connections
-clusterConnect(basket_cells, granule_cells)
+for i in range(N_clusters):
+  clusterConnect(basket_cells, granule_cells, N_clusters, i)
+  clusterConnect(basket_cells, mossy_cells, N_clusters, i)
 # nest.Connect(basket_cells, mossy_cells)
 
 # Mossy cells connections
-# nest.Connect(mossy_cells, granule_cells)
+for i in range(N_clusters):
+  clusterConnect(mossy_cells, granule_cells, N_clusters, i)
 # nest.Connect(mossy_cells, basket_cells)
 # nest.Connect(mossy_cells, hipp_cells)
 
 # HIPP cells connections
-# nest.Connect(hipp_cells, granule_cells)
+for i in range(N_clusters):
+  clusterConnect(hipp_cells, granule_cells, N_clusters, i)
 # nest.Connect(hipp_cells, basket_cells)
 # nest.Connect(hipp_cells, mossy_cells)
 
@@ -42,10 +51,7 @@ clusterConnect(basket_cells, granule_cells)
 # connections = nest.GetConnections(granule_cells, basket_cells)
 # print(f"Total number of connections: {len(connections)}")
 
-for i, bc in enumerate(basket_cells[:10]):
-  incoming = nest.GetConnections(target=bc)
-  print(f"Basket cell {i + 1} has {len(incoming)} incoming connections")
-
-for i, gc in enumerate(granule_cells[:10]):
-  incoming = nest.GetConnections(source=gc)
-  print(f"Granule cell {i + 1} has {len(incoming)} outgoing connections")
+# printConnections(granule_cells, target=True)
+# printConnections(granule_cells, target=False)
+# printConnections(basket_cells, target=True)
+# printConnections(basket_cells, target=False)
--- src/utils/connect.py
+++ src/utils/connect.py
@@ -1,19 +1,32 @@
 import nest
 
 
-def clusterConnect(from_pop, to_pop, syn_spec=None):
-  len_from_pop = len(from_pop)
-  len_to_pop = len(to_pop)
+def clusterConnect(from_pop, to_pop, n_clusters, cluster_index, syn_spec=None):
+  from_per_cluster = len(from_pop) // n_clusters
+  to_per_cluster = len(to_pop) // n_clusters
+  from_index = cluster_index * from_per_cluster
+  to_index = cluster_index * to_per_cluster
 
-  if len_from_pop < len_to_pop:
-    if len_to_pop % len_from_pop != 0:
-      raise ValueError("The number of source cells must be a multiple of the number of target cells")
+  nest.Connect(
+      from_pop[from_index:from_index + from_per_cluster],
+      to_pop[to_index:to_index + to_per_cluster],
+      'all_to_all',
+      syn_spec,
+  )
 
-    for i in range(len_from_pop):
-      nest.Connect(from_pop[i], to_pop[i::len_from_pop], 'all_to_all', syn_spec)
-  else:
-    if len_from_pop % len_to_pop != 0:
-      raise ValueError("The number of target cells must be a multiple of the number of source cells")
+# def clusterConnect(from_pop, to_pop, syn_spec=None):
+#   len_from_pop = len(from_pop)
+#   len_to_pop = len(to_pop)
 
-    for i in range(len_to_pop):
-      nest.Connect(from_pop[i::len_to_pop], to_pop[i], 'all_to_all', syn_spec)
+#   if len_from_pop < len_to_pop:
+#     if len_to_pop % len_from_pop != 0:
+#       raise ValueError("The number of source cells must be a multiple of the number of target cells")
+
+#     for i in range(len_from_pop):
+#       nest.Connect(from_pop[i], to_pop[i::len_from_pop], 'all_to_all', syn_spec)
+#   else:
+#     if len_from_pop % len_to_pop != 0:
+#       raise ValueError("The number of target cells must be a multiple of the number of source cells")
+
+#     for i in range(len_to_pop):
+#       nest.Connect(from_pop[i::len_to_pop], to_pop[i], 'all_to_all', syn_spec)
