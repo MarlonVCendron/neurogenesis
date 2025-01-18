b5687fbae79c1eba28c36219f9ce982b307c16da
Marlon <marlonvcendron@gmail.com>
2025-01-18 11:56:28 -0300

    bc

--- neurogenesis/models/cells/bc.py
+++ neurogenesis/models/cells/bc.py
@@ -1,23 +1,29 @@
 from brian2 import *
 from neurogenesis.models.general.lif import LIF
 
+params = {
+    "Cm"        : 232.6 * pF,
+    "g_L"       : 23.2 * nS,
+    "E_L"       : -62.0 * mV,
+    "g_ahp_max" : 76.9 * nS,
+    "tau_ahp"   : 2 * ms,
+    "E_ahp"     : -75.0 * mV,
+    "V_th"      : -52.5 * mV,
+    "I_syn"     : -250 * pA,
+}
+
 lif_eqs = LIF()
+
 bc = NeuronGroup(
-    1,
-    model=lif_eqs,
-    threshold='Vm > V_th',
-    reset='Vm = E_L',
-    method='euler',
-    refractory=0*ms  # A way to have lastspike
+    10,
+    model      = lif_eqs,
+    threshold  = 'Vm > V_th',
+    reset      = 'Vm = E_L',
+    method     = 'euler',
+    refractory = 0*ms          # A way to have lastspike
 )
-bc.Cm        = 232.6 * pF
-bc.g_L       = 23.2 * nS
-bc.E_L       = -62.0 * mV
-bc.g_ahp_max = 76.9 * nS
-bc.tau_ahp   = 2 * ms
-bc.E_ahp     = -75.0 * mV
-bc.V_th      = -52.5 * mV
-bc.I_syn     = -250 * pA
+for param, value in params.items():
+  setattr(bc, param, value)
 
 bc.Vm = bc.E_L
 
