039c4d6e703402a9b9289d4cc6f7364dcc1fb6db
Marlon <marlonvcendron@gmail.com>
2025-01-18 13:16:20 -0300

    lif I_syn

--- neurogenesis/models/general/lif.py
+++ neurogenesis/models/general/lif.py
@@ -5,11 +5,8 @@ def LIF():
     dVm/dt = (-I_L -I_ahp -I_syn) / Cm                      : volt
     I_L    = g_L * (Vm - E_L)                               : amp
     I_ahp  = g_ahp * (Vm - E_ahp)                           : amp
-    g_ahp  = g_ahp_max * e ** (- (t - lastspike) / tau_ahp) : siemens
-    # I_syn   = I_ampa + I_nmda + I_gaba                     : amp
-    # I_ampa  = g_ampa * (Vm - E_ampa)                       : amp
-    # I_nmda  = g_nmda * (Vm - E_nmda)                       : amp
-    # I_gaba  = g_gaba * (Vm - E_gaba)                       : amp
+    g_ahp  = g_ahp_max * exp(- (t - lastspike) / tau_ahp) : siemens
+    I_syn  = I_ampa + I_nmda + I_gaba                       : amp
   ''')
 
   eq_params = Equations('''
@@ -20,7 +17,9 @@ def LIF():
     tau_ahp   : second   # AHP time constant
     E_ahp     : volt     # AHP reversal potential
     V_th      : volt     # Threshold potential
-    I_syn     : amp
+    I_ampa    : amp      # Synaptic current (AMPA)
+    I_nmda    : amp      # Synaptic current (NMDA)
+    I_gaba    : amp      # Synaptic current (GABA)
   ''')
 
   eqs = eq_model + eq_params
