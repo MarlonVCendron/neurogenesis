7de57093c180d43317a06d0f32f1aa8e9cb36160
Marlon <marlonvcendron@gmail.com>
2025-01-20 10:18:16 -0300

    igc params

--- neurogenesis/models/cells/igc.py
+++ neurogenesis/models/cells/igc.py
@@ -4,10 +4,10 @@ from neurogenesis.models.general.lif import LIF
 params = {
     "Cm"        : 106.2 * pF,
     "g_L"       : 3.4 * nS,
-    "E_L"       : -75.0 * mV,
+    "E_L"       : -72.0 * mV,
     "g_ahp_max" : 10.4 * nS,
     "tau_ahp"   : 20.0 * ms,
-    "E_ahp"     : -80.0 * mV,
+    "E_ahp"     : -72.0 * mV,
     "V_th"      : -53.4 * mV,
     "I_ampa"    : 0 * pA,
     "I_nmda"    : 0 * pA,
