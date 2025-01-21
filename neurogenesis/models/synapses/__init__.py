e8c0ca1b4b617279ad7e7d905a8f773dd3b3ab0a
Marlon <marlonvcendron@gmail.com>
2025-01-21 10:37:25 -0300

    inits

--- neurogenesis/models/synapses/__init__.py
+++ neurogenesis/models/synapses/__init__.py
@@ -1,3 +1,4 @@
 from neurogenesis.models.synapses.ampa import AMPA
 from neurogenesis.models.synapses.nmda import NMDA
 from neurogenesis.models.synapses.gaba import GABA
+from neurogenesis.models.synapses.synapse import synapse_model
--- neurogenesis/util.py
+++
@@ -1,7 +0,0 @@
-# Condition to connect cells in lamellae
-def lamellar_conn(N_i, N_j):
-  return f'i // {N_i} == j // {N_j}'
-
-# Condition to connect cells cross lamellae
-def cross_lamellar_conn(N_i, N_j):
-  return f'i // {N_i} != j // {N_j}'
