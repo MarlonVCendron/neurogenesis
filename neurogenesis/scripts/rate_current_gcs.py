bbfe7186302541430d4425a6c4183138e94c8227
Marlon <marlonvcendron@gmail.com>
2025-01-24 13:55:35 -0300

    save fig rate current

--- neurogenesis/scripts/rate_current_gcs.py
+++ neurogenesis/scripts/rate_current_gcs.py
@@ -45,4 +45,6 @@ ax.plot([curr for curr, _, _ in rates], [igc_rate for _, _, igc_rate in rates], 
 ax.set_xlabel('Current (pA)')
 ax.set_ylabel('Frequency (Hz)')
 ax.legend()
-plt.show()
+# plt.show()
+plt.savefig('neurogenesis/figures/rate_current_gcs.png')
+plt.close()
