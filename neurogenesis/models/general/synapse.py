6e99c6b8c2b15569a85b39becc75a5b25f556a84
Marlon <marlonvcendron@gmail.com>
2025-01-24 09:36:02 -0300

    correct syn eq?

--- neurogenesis/models/general/synapse.py
+++ neurogenesis/models/general/synapse.py
@@ -5,26 +5,23 @@ def synapse(receptor):
   R = receptor
 
   eq_model = Equations(f'''
-    I_synapse  = g * (Vm - E)                  : amp
-    g          = K * s                         : siemens
-    s          = (y_d - y_r) / (tau_d - tau_r) : Hz
-    dy_r/dt    = -y_r / tau_r                  : 1 (clock-driven)
-    dy_d/dt    = -y_d / tau_d                  : 1 (clock-driven)
-    I_{R}_post = I_synapse                     : amp (summed)
+    I_synapse    = g * (Vm - E)                : amp
+    g            = K * s                       : siemens
+    ds/dt        = -s / tau_d + h0 * z * (1 - s) : 1 (clock-driven)
+    dz/dt        = -z / tau_r                  : 1 (clock-driven)
+    I_{R}_post   = I_synapse                   : amp (summed)
   ''')
 
-  eq_params = Equations(f'''
-    K     : siemens/hertz  # Synaptic strength
-    E     : volt           # Reversal potential
-    tau_r : second         # Rise time
-    tau_d : second         # Decay time
-    w     = 1 : 1          # Synaptic weight
+  eq_params = Equations('''
+    K     : siemens             # Synaptic strength
+    E     : volt                # Reversal potential
+    tau_r : second              # Rise time
+    tau_d : second              # Decay time
+    w     = 1          : 1      # Synaptic weight
+    h0    = 1 * ms**-1 : hertz  # TODO: this is a param
   ''')
 
-  on_pre = f'''
-    y_r += w
-    y_d += w
-  '''
+  on_pre = 'z += w'
 
   eqs = eq_model + eq_params
 
