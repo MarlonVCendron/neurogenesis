dd39b15481cf63e2ce553a07b905307262dedbc5
Marlon <marlonvcendron@gmail.com>
2025-02-03 09:56:55 -0300

    indent

--- models/general/synapse.py
+++ models/general/synapse.py
@@ -4,14 +4,14 @@ def synapse(receptor):
   R = receptor
 
   eq_model = Equations(f'''
-    I_synapse = g_syn * (Vm - E)                                       : amp
-    g_syn     = g_max * f  * g                                         : siemens
-    dg/dt     = -g/tau_d + h * 1/ms                                    : 1 (clock-driven)
-    dh/dt     = -h/tau_r                                               : 1 (clock-driven)
-    t_peak    = (tau_d * tau_r / (tau_d - tau_r)) * log(tau_d / tau_r) : second
-    f         = 1 / (-exp(-t_peak / tau_r) + exp(-t_peak / tau_d))     : 1
-    
-    I_{R}_post   = I_synapse                     : amp (summed)
+    I_synapse  = g_syn * (Vm - E)                                       : amp
+    g_syn      = g_max * f  * g                                         : siemens
+    dg/dt      = -g/tau_d + h * 1/ms                                    : 1 (clock-driven)
+    dh/dt      = -h/tau_r                                               : 1 (clock-driven)
+    t_peak     = (tau_d * tau_r / (tau_d - tau_r)) * log(tau_d / tau_r) : second
+    f          = 1 / (-exp(-t_peak / tau_r) + exp(-t_peak / tau_d))     : 1
+
+    I_{R}_post = I_synapse                                              : amp (summed)
   ''')
 
   eq_params = Equations('''
