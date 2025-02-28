dd0aadf39dc21aa1b676678d6f732e3f1d1fc161
Marlon <marlonvcendron@gmail.com>
2025-02-28 15:25:56 -0300

    extra conns

--- utils/connections.py
+++ utils/connections.py
@@ -5,3 +5,10 @@ def lamellar_conn(N_i, N_j):
 # Condition to connect cells cross lamellae
 def cross_lamellar_conn(N_i, N_j):
   return f'i // {N_i} != j // {N_j}'
+
+
+# def cross_lamellar_conn(N_i, N_j):
+#   return f'(abs((i // {N_i}) - (j // {N_j})) % 20 == 1)'
+
+# def range_conn(N_i, N_j, N=20, d=4):
+#   return f'(abs((i // {N_i}) - (j // {N_j})) % {N} < {d})'
