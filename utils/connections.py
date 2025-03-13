6d4be67b208027da6bed605f21aeec30c9244ad7
Marlon <marlonvcendron@gmail.com>
2025-03-13 06:46:58 -0300

    correct cross

--- utils/connections.py
+++ utils/connections.py
@@ -3,12 +3,11 @@ def lamellar_conn(N_i, N_j):
   return f'i // {N_i} == j // {N_j}'
 
 # Condition to connect cells cross lamellae
-# def cross_lamellar_conn(N_i, N_j):
-#   return f'i // {N_i} != j // {N_j}'
-
-
 def cross_lamellar_conn(N_i, N_j):
-  return f'(abs((i // {N_i}) - (j // {N_j})) % 20 == 1)'
+  return f'i // {N_i} != j // {N_j}'
+
+# def cross_lamellar_conn(N_i, N_j):
+#   return f'(abs((i // {N_i}) - (j // {N_j})) % 20 == 1)'
 
 # def range_conn(N_i, N_j, N=20, d=4):
 #   return f'(abs((i // {N_i}) - (j // {N_j})) % {N} < {d})'
