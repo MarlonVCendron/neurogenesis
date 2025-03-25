57e9ad2c2c71c31f27a1877a19b20deff3deda86
Marlon <marlonvcendron@gmail.com>
2025-03-25 09:22:43 -0300

    remove comments

--- models/general/network.py
+++ models/general/network.py
@@ -13,7 +13,6 @@ from models.cells import (
 
 
 def network():
-  # Cells
   pp   = create_pp(N=cell_params['pp']['N'])
   mgc  = create_mgc()
   igc  = create_igc()
@@ -25,8 +24,6 @@ def network():
   pp_nmda_mgc = Connect(pp, mgc, **syn_params['pp_nmda_mgc'])
   pp_ampa_igc = Connect(pp, igc, **syn_params['pp_ampa_igc'])
   pp_nmda_igc = Connect(pp, igc, **syn_params['pp_nmda_igc'])
-  # pp_ampa_bc  = Connect(pp, bc, **syn_params['pp_ampa_bc'])
-  # pp_nmda_bc  = Connect(pp, bc, **syn_params['pp_nmda_bc'])
   pp_ampa_hipp = Connect(pp, hipp, **syn_params['pp_ampa_hipp'])
   pp_nmda_hipp = Connect(pp, hipp, **syn_params['pp_nmda_hipp'])
 
@@ -34,15 +31,11 @@ def network():
   mgc_nmda_bc   = Connect(mgc, bc, **syn_params['mgc_nmda_bc'])
   mgc_ampa_mc   = Connect(mgc, mc, **syn_params['mgc_ampa_mc'])
   mgc_nmda_mc   = Connect(mgc, mc, **syn_params['mgc_nmda_mc'])
-  # mgc_ampa_hipp = Connect(mgc, hipp, **syn_params['mgc_ampa_hipp'])
-  # mgc_nmda_hipp = Connect(mgc, hipp, **syn_params['mgc_nmda_hipp'])
 
   igc_ampa_bc   = Connect(igc, bc, **syn_params['igc_ampa_bc'])
   igc_nmda_bc   = Connect(igc, bc, **syn_params['igc_nmda_bc'])
   igc_ampa_mc   = Connect(igc, mc, **syn_params['igc_ampa_mc'])
   igc_nmda_mc   = Connect(igc, mc, **syn_params['igc_nmda_mc'])
-  # igc_ampa_hipp = Connect(igc, hipp, **syn_params['igc_ampa_hipp'])
-  # igc_nmda_hipp = Connect(igc, hipp, **syn_params['igc_nmda_hipp'])
 
   bc_gaba_mgc = Connect(bc, mgc, **syn_params['bc_gaba_mgc'])
   bc_gaba_mc  = Connect(bc, mc, **syn_params['bc_gaba_mc'])
@@ -53,12 +46,8 @@ def network():
   mc_nmda_igc  = Connect(mc, igc, **syn_params['mc_nmda_igc'])
   mc_ampa_bc   = Connect(mc, bc, **syn_params['mc_ampa_bc'])
   mc_nmda_bc   = Connect(mc, bc, **syn_params['mc_nmda_bc'])
-  # mc_ampa_hipp = Connect(mc, hipp, **syn_params['mc_ampa_hipp'])
-  # mc_nmda_hipp = Connect(mc, hipp, **syn_params['mc_nmda_hipp'])
 
   hipp_gaba_mgc = Connect(hipp, mgc, **syn_params['hipp_gaba_mgc'])
-  # hipp_gaba_bc  = Connect(hipp, bc, **syn_params['hipp_gaba_bc'])
-  # hipp_gaba_mc  = Connect(hipp, mc, **syn_params['hipp_gaba_mc'])
 
   net = Network(collect())
 
