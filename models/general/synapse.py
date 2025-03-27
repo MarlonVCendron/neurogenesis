2addc055576093ad386f6f09913e569d17ffecc5
Marlon <marlonvcendron@gmail.com>
2025-03-27 07:55:56 -0300

    fix

--- models/general/synapse.py
+++ models/general/synapse.py
@@ -1,9 +1,9 @@
 from brian2 import *
 from utils.args_config import args
 
-alpha_nmda = (args.nmda or 11) * ms  # NMDA scaling factor
-alpha_ampa = (args.ampa or 5) * ms   # AMPA scaling factor
-alpha_gaba = (args.gaba or 6) * ms   # GABA scaling factor
+alpha_nmda = args.nmda or 11  # NMDA scaling factor
+alpha_ampa = args.ampa or 5   # AMPA scaling factor
+alpha_gaba = args.gaba or 6   # GABA scaling factor
 
 def synapse(receptor):
   R = receptor
@@ -18,18 +18,19 @@ def synapse(receptor):
   ''')
 
   eq_params = Equations('''
-    g_max : siemens         # Synaptic strength
-    E     : volt            # Reversal potential
-    tau_r : second          # Rise time
-    tau_d : second          # Decay time
-    w     = 1     : 1       # Synaptic weight
+    g_max : siemens  # Synaptic strength
+    E     : volt     # Reversal potential
+    tau_r : second   # Rise time
+    tau_d : second   # Decay time
+    w     = 1 : 1    # Synaptic weight
   ''')
 
   on_pre = 'h += w'
 
-  is_nmda  = R.startswith('nmda')
-  is_ampa  = R.startswith('ampa')
-  eq_alpha = Equations(f'alpha = {alpha_nmda if is_nmda else alpha_ampa if is_ampa else alpha_gaba} : second')
+  is_nmda   = R.startswith('nmda')
+  is_ampa   = R.startswith('ampa')
+  alpha_val = alpha_nmda if is_nmda else alpha_ampa if is_ampa else alpha_gaba
+  eq_alpha  = Equations(f'alpha = {alpha_val}*ms : second')
 
   eqs = eq_model + eq_params + eq_alpha
 
