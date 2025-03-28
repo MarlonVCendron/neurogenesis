7971621babf590387791fa70a45126f0da4dd61f
Marlon <marlonvcendron@gmail.com>
2025-03-27 23:30:51 -0300

    res filename

--- main.py
+++ main.py
@@ -8,6 +8,13 @@ from sim import SimWrapper
 from params import results_dir, trials
 from utils.args_config import args
 
+def res_filename(i, total):
+  patterns_per_trial = total // trials
+  trial_index = i // patterns_per_trial
+  pattern_index = i % patterns_per_trial
+  flag = 'neurogenesis' if args.neurogenesis else 'control'
+  return f'{flag}_trial_{trial_index}_pattern_{pattern_index}'
+
 if __name__ == '__main__':
   initialize()
 
@@ -17,9 +24,10 @@ if __name__ == '__main__':
   if args.single_run:
     patterns    = patterns[:1]
 
-  result_dirs = [join(results_dir, f'{i}') for i in range(len(patterns))]
+  total_patterns = len(patterns)
+  result_dirs = [join(results_dir, res_filename(i, total_patterns)) for i in range(total_patterns)]
 
   results = tqdm_pathos.starmap(sim.do_run, zip(patterns, result_dirs))
 
-  for i, (spikes, rates) in enumerate(results):
-    plot_spikes_and_rates(spikes, rates, i)
+  # for i, (spikes, rates) in enumerate(results):
+  #   plot_spikes_and_rates(spikes, rates, i)
