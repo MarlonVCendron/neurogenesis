82282b0db36b7efb1d6292d4c025c80af176ea0a
Marlon <marlonvcendron@gmail.com>
2025-04-01 08:05:17 -0300

    process dg

--- scripts/process_dg.py
+++ scripts/process_dg.py
@@ -6,8 +6,8 @@ import matplotlib.pyplot as plt
 from params import results_dir
 from utils.patterns import pattern_separation_degree, activation_degree, correlation_degree
 
-run_01_dir = os.path.join(results_dir, 'run_01')
-subdirs = [os.path.join(run_01_dir, d) for d in os.listdir(run_01_dir) if os.path.isdir(os.path.join(run_01_dir, d))]
+run_02_dir = os.path.join(results_dir, 'run_02')
+subdirs = [os.path.join(run_02_dir, d) for d in os.listdir(run_02_dir) if os.path.isdir(os.path.join(run_02_dir, d))]
 
 data = {}
 
@@ -17,14 +17,17 @@ for subdir in subdirs:
 
   if os.path.exists(file_path):
     with h5py.File(file_path, 'r') as h5file:
+      parts = run_name.split('_')
+      pattern = int(parts[-1])
+      trial = int(parts[-3])
+      group = '_'.join(parts[:-4]) # 'control' or 'neurogenesis_0.2'
+
+      has_neurogenesis = 'neurogenesis' in group
+
       pp_pattern = np.array(h5file['pp_pattern'])
       mgc_pattern = np.array(h5file['mgc_pattern'])
+      igc_pattern = np.array(h5file['igc_pattern']) if has_neurogenesis else np.array([])
       in_similarity = np.array(h5file['in_similarity']).item()
-
-      parts = run_name.split('_')
-      group = parts[0]  # 'control' or 'neurogenesis'
-      trial = int(parts[2])
-      pattern = int(parts[-1])
 
       if group not in data:
         data[group] = []
@@ -36,7 +39,9 @@ for subdir in subdirs:
       pattern = {
           'pp_pattern': pp_pattern,
           'mgc_pattern': mgc_pattern,
-          'in_similarity': in_similarity
+          'igc_pattern': igc_pattern,
+          'in_similarity': in_similarity,
+          'gc_pattern': np.concatenate((mgc_pattern, igc_pattern)) 
       }
 
       if (in_similarity == 1.0):
