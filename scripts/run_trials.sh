5d33a22649cabf6067eb29593f9e7a430f9716c3
Marlon <marlonvcendron@gmail.com>
2025-04-01 08:45:38 -0300

    fix run trials

--- scripts/run_trials.sh
+++ scripts/run_trials.sh
@@ -8,6 +8,6 @@ python -m main --trials $trials --prefix $prefix
 
 for igc_conn in $(seq 0.1 0.1 1.0); do
   echo "Running: neurogenesis --igc-conn $igc_conn"
-  python -m main --trials $trials --neurogenesis --igc-conn=$igc_conn
+  python -m main --trials $trials --neurogenesis --igc-conn=$igc_conn --prefix $prefix
 done
 
