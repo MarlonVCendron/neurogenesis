2ec01f208cc4acd383b036123c2a508f8909b907
Marlon <marlonvcendron@gmail.com>
2025-05-26 10:15:15 -0300

    visualize connectivity

--- plotting/visualize_connectivity.py
+++ plotting/visualize_connectivity.py
@@ -1,27 +1,37 @@
 import h5py
-import networkx as nx
 import numpy as np
 import matplotlib.pyplot as plt
 from collections import defaultdict
 from scipy.sparse import csr_matrix
-
+from utils.utils import read_connectivity
 from os.path import join
 from params import results_dir
 
 
-filename = join(results_dir, "connectivity_matrices.h5")
+# old
+
+# filename = join(results_dir, "connectivity_matrices.h5")
+# with h5py.File(filename, 'r') as f:
+#   group_name = "bc->mgc"
+#   row = f[f"{group_name}/row"][:]
+#   col = f[f"{group_name}/col"][:]
+#   data = f[f"{group_name}/data"][:]
+
+#   shape = (max(row) + 1, max(col) + 1)
+#   sparse_matrix = csr_matrix((data, (row, col)), shape=shape)
+#   print(np.mean(np.sum(sparse_matrix, axis=1)))
+#   print(np.mean(np.sum(sparse_matrix, axis=0)))
 
 
-with h5py.File(filename, 'r') as f:
-  group_name = "bc->mgc"
-  row = f[f"{group_name}/row"][:]
-  col = f[f"{group_name}/col"][:]
-  data = f[f"{group_name}/data"][:]
+conn_i, conn_j = read_connectivity("pp", "hipp")
 
-  shape = (max(row) + 1, max(col) + 1)
-  sparse_matrix = csr_matrix((data, (row, col)), shape=shape)
-  print(np.mean(np.sum(sparse_matrix, axis=1)))
-  print(np.mean(np.sum(sparse_matrix, axis=0)))
+shape = (max(conn_i) + 1, max(conn_j) + 1)
+sparse_matrix = csr_matrix((np.ones(len(conn_i)), (conn_i, conn_j)), shape=shape)
+
+connection_probability_from = np.mean(np.sum(sparse_matrix, axis=1)) / shape[1]
+connection_probability_to = np.mean(np.sum(sparse_matrix, axis=0)) / shape[0]
+print(connection_probability_from)
+print(connection_probability_to)
 
 plt.imshow(sparse_matrix.toarray(), aspect="auto", cmap="Greys", interpolation="nearest")
 plt.show()
