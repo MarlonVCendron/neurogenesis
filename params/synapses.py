669d813cb81b9bc3d6da98fb77ca9e9c58a18500
Marlon <marlonvcendron@gmail.com>
2025-06-02 10:19:49 -0300

    good?

--- params/synapses.py
+++ params/synapses.py
@@ -1,13 +1,14 @@
 from brian2 import *
-from utils.connections import lamellar_conn
+from utils.connections import lamellar_conn, cross_lamellar_conn
 from params.general import igc_conn
-from params.cells import N_mgc_l, N_igc_l, N_bc_l, N_pca3_l, N_ica3_l
+from params.cells import N_mgc_l, N_igc_l, N_bc_l, N_pca3_l, N_ica3_l, N_mc_l, N_hipp_l
 
 syn_params = {
     "pp_mgc": {
         "syn_type" : "exc",
         "syn_var"  : 4,
-        "p"        : 0.0085,
+        # "p"        : 0.0085,
+        "p"        : 0.08,
         "g"        : 1.8246 * nS,
         "tau_r"    : 266.2388 * ms,
         "tau_d"    : 5.3332 * ms,
@@ -15,10 +16,23 @@ syn_params = {
         "U_se"     : 0.2697,
         "delay"    : 1.0 * ms
     },
+    "pp_mc": { # From MEC LII Stellate to DG Mossy MOLDEN
+        "syn_type" : "exc",
+        "syn_var"  : 1,
+        # "p"        : 0.0085,
+        "p"        : 0.25,
+        "g"        : 1.4222 * nS,
+        "tau_d"    : 4.6711 * ms,
+        "tau_r"    : 319.8348 * ms,
+        "tau_f"    : 57.7657 * ms,
+        "U_se"     : 0.2041,
+        "delay"    : 1.0 * ms
+    },
     "pp_bc": {
         "syn_type" : "exc",
         "syn_var"  : 5,
-        "p"        : 0.0256,
+        # "p"        : 0.0256,
+        "p"        : 0.2,
         "g"        : 1.4058 * nS,
         "tau_r"    : 144.4152 * ms,
         "tau_d"    : 3.8495 * ms,
@@ -48,10 +62,13 @@ syn_params = {
         "U_se"     : 0.1946,
         "delay"    : 1.0 * ms
     },
+
     "mgc_mc": {
         "syn_type" : "exc",
-        "syn_var"  : 1,
-        "p"        : 0.0078,
+        "syn_var"  : 2,
+        # "p"        : 0.0078,
+        "p"        : 0.2,
+        "condition": lamellar_conn(N_mgc_l, N_mc_l),
         "g"        : 1.7130 * nS,
         "tau_r"    : 428.5826 * ms,
         "tau_d"    : 5.3469 * ms,
@@ -62,7 +79,8 @@ syn_params = {
     "mgc_hipp": {
         "syn_type" : "exc",
         "syn_var"  : 2,
-        "p"        : 0.0059,
+        # "p"        : 0.0059,
+        "p"        : 0.1,
         "g"        : 1.3047 * nS,
         "tau_r"    : 462.8138 * ms,
         "tau_d"    : 5.1814 * ms,
@@ -73,7 +91,9 @@ syn_params = {
     "mgc_bc": {
         "syn_type" : "exc",
         "syn_var"  : 2,
-        "p"        : 0.0035,
+        # "p"        : 0.0035,
+        "p"        : 1.0,
+        "condition": lamellar_conn(N_mgc_l, N_bc_l),
         "g"        : 1.4575 * nS,
         "tau_r"    : 151.2653 * ms,
         "tau_d"    : 3.5662 * ms,
@@ -103,10 +123,13 @@ syn_params = {
         "U_se"     : 0.1641,
         "delay"    : 1.0 * ms
     },
+
     "mc_mgc": {
         "syn_type" : "exc",
         "syn_var"  : 3,
-        "p"        : 0.0024,
+        # "p"        : 0.0024,
+        "p"        : 0.002,
+        "condition": cross_lamellar_conn(N_mc_l, N_mgc_l),
         "g"        : 2.3943 * nS,
         "tau_r"    : 166.1625 * ms,
         "tau_d"    : 5.3570 * ms,
@@ -116,8 +139,10 @@ syn_params = {
     },
     "mc_mc": {
         "syn_type" : "exc",
-        "syn_var"  : 2,
-        "p"        : 0.0122,
+        "syn_var"  : 3,
+        # "p"        : 0.0122,
+        "p"        : 0.1,
+        "condition": cross_lamellar_conn(N_mc_l, N_mc_l),
         "g"        : 2.0675 * nS,
         "tau_r"    : 249.3294 * ms,
         "tau_d"    : 4.2571 * ms,
@@ -128,7 +153,10 @@ syn_params = {
     "mc_hipp": {
         "syn_type" : "exc",
         "syn_var"  : 3,
-        "p"        : 0.0099,
+        # "p"        : 0.0099,
+        "p"        : 1.0,
+        # "condition": cross_lamellar_conn(N_mc_l, N_hipp_l),
+        "condition": lamellar_conn(N_mc_l, N_hipp_l),
         "g"        : 1.3762 * nS,
         "tau_r"    : 358.4310 * ms,
         "tau_d"    : 4.8235 * ms,
@@ -139,7 +167,9 @@ syn_params = {
     "mc_bc": {
         "syn_type" : "exc",
         "syn_var"  : 4,
-        "p"        : 0.0117,
+        # "p"        : 0.0117,
+        "p"        : 1.0,
+        "condition": cross_lamellar_conn(N_mc_l, N_bc_l),
         "g"        : 1.9956 * nS,
         "tau_r"    : 117.3654 * ms,
         "tau_d"    : 3.3955 * ms,
@@ -147,10 +177,12 @@ syn_params = {
         "U_se"     : 0.2545,
         "delay"    : 1.0 * ms
     },
+
     "hipp_mgc": {
         "syn_type" : "inh",
         "syn_var"  : 2,
-        "p"        : 0.0060,
+        # "p"        : 0.0060,
+        "p"        : 0.2,
         "g"        : 2.0020 * nS,
         "tau_r"    : 559.1428 * ms,
         "tau_d"    : 8.9349 * ms,
@@ -161,7 +193,8 @@ syn_params = {
     "hipp_bc": {
         "syn_type" : "inh",
         "syn_var"  : 3,
-        "p"        : 0.0061,
+        # "p"        : 0.0061,
+        "p"        : 0.02,
         "g"        : 1.7092 * nS,
         "tau_r"    : 367.1976 * ms,
         "tau_d"    : 5.9816 * ms,
@@ -169,10 +202,13 @@ syn_params = {
         "U_se"     : 0.2209,
         "delay"    : 1.0 * ms
     },
+
     "bc_mgc": {
         "syn_type" : "inh",
         "syn_var"  : 1,
-        "p"        : 0.0064,
+        # "p"        : 0.0064,
+        "p"        : 1.0,
+        "condition": lamellar_conn(N_bc_l, N_mgc_l),
         "g"        : 2.4509 * nS,
         "tau_r"    : 433.8758 * ms,
         "tau_d"    : 6.5435 * ms,
@@ -183,7 +219,8 @@ syn_params = {
     "bc_hipp": {
         "syn_type" : "inh",
         "syn_var"  : 1,
-        "p"        : 0.0044,
+        # "p"        : 0.0044,
+        "p"        : 0.02,
         "g"        : 1.4078 * nS,
         "tau_r"    : 534.1822 * ms,
         "tau_d"    : 6.5442 * ms,
@@ -194,7 +231,8 @@ syn_params = {
     "bc_bc": {
         "syn_type" : "inh",
         "syn_var"  : 1,
-        "p"        : 0.0044,
+        # "p"        : 0.0044,
+        "p"        : 0.2,
         "g"        : 1.9814 * nS,
         "tau_r"    : 195.5361 * ms,
         "tau_d"    : 4.6481 * ms,
@@ -202,6 +240,7 @@ syn_params = {
         "U_se"     : 0.3095,
         "delay"    : 1.0 * ms
     },
+
     "pca3_pca3": {
         "syn_type" : "exc",
         "syn_var"  : 2,
@@ -224,6 +263,7 @@ syn_params = {
         "U_se"     : 0.1671,
         "delay"    : 1.0 * ms
     },
+
     "ica3_pca3": {
         "syn_type" : "inh",
         "syn_var"  : 1,
