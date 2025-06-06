8782cb878c117e498ceae7958ef1f16ff4d882eb
Marlon <marlonvcendron@gmail.com>
2025-06-06 09:41:40 -0300

    table

--- plotting/visualize_connectivity.py
+++ plotting/visualize_connectivity.py
@@ -23,7 +23,7 @@ from params import results_dir
 #   print(np.mean(np.sum(sparse_matrix, axis=0)))
 
 
-conn_i, conn_j = read_connectivity("mgc", "mc")
+conn_i, conn_j = read_connectivity("pca3", "mc")
 
 shape = (max(conn_i) + 1, max(conn_j) + 1)
 sparse_matrix = csr_matrix((np.ones(len(conn_i)), (conn_i, conn_j)), shape=shape)
@@ -32,6 +32,14 @@ connection_probability_from = np.mean(np.sum(sparse_matrix, axis=1)) / shape[1]
 connection_probability_to = np.mean(np.sum(sparse_matrix, axis=0)) / shape[0]
 print(connection_probability_from)
 print(connection_probability_to)
+
+min_from = np.sum(sparse_matrix, axis=1).min()
+max_from = np.sum(sparse_matrix, axis=1).max()
+min_to = np.sum(sparse_matrix, axis=0).min()
+max_to = np.sum(sparse_matrix, axis=0).max()
+
+print(f"min from: {min_from}, max from: {max_from}")
+print(f"min to: {min_to}, max to: {max_to}")
 
 plt.imshow(sparse_matrix.toarray(), aspect="auto", cmap="Greys", interpolation="nearest")
 plt.show()
