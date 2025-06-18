377ea71d0a1b32ee71624f009057e1793c725895
Marlon <marlonvcendron@gmail.com>
2025-06-18 08:37:49 -0300

    fix synaptic model, definitive

--- models/general/synapse_tsodyks.py
+++ models/general/synapse_tsodyks.py
@@ -6,19 +6,18 @@ def synapse_tsodyks(syn_type, syn_var) :
   eq_reversal = Equations(f'E = {E_val} * mV : volt')
 
   eq_model = Equations(f'''
-    dx/dt     = z / tau_r                    : 1 (clock-driven)  # Recovered
-    dy/dt     = -y / tau_d                   : 1 (clock-driven)  # Active
-    dv/dt     = -v / tau_f                   : 1 (clock-driven)  # Facilitation
-    z         = 1 - x - y                    : 1                 # Inactive
-    I_synapse = scale * w * g * y * (Vm - E) : amp
+    dv/dt = -v / tau_f               : 1 (clock-driven)    # Resource utilization
+    dx/dt = (1 - x) / tau_r          : 1 (clock-driven)    # Resource recovery
+    dI/dt = -I / tau_d               : amp (clock-driven)  # Synaptic current
+    A     = scale * w * g * (Vm - E) : amp
 
-    I_syn_{syn_var}_post = I_synapse : amp (summed)
+    I_syn_{syn_var}_post = I : amp (summed)
   ''')
 
   eq_params = Equations('''
     g     : siemens  # Synaptic conductance
     U_se  : 1        # Portion of available resources utilized on each synaptic event 
-    tau_d : second   # Decay time constant
+    tau_d : second   # Decay time constant (synaptic current)
     tau_r : second   # Resource recovery time constant
     tau_f : second   # Resource utilization reduction time constant
     scale : 1        # Scale factor
@@ -28,8 +27,8 @@ def synapse_tsodyks(syn_type, syn_var) :
   # Order is important here
   on_pre = '''
     v += U_se * (1 - v)
-    y += v * x
     x += -v * x
+    I += v * x * A
   '''
 
   eqs = eq_model + eq_params + eq_reversal
