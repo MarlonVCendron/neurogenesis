80b2fa313bfea0a97bd38733d7d7904669e51f92
Marlon <marlonvcendron@gmail.com>
2025-07-12 10:50:22 -0300

    intro

--- scripts/generate_all_connectivities.sh
+++ scripts/generate_all_connectivities.sh
@@ -8,5 +8,5 @@ python -m scripts.generate_connectivity_matrices --skip-conn --no-neurogenesis
 
 for igc_conn in $(seq 0.1 0.1 1.0); do
   echo "Running: neurogenesis --igc-conn $igc_conn"
-  python -m scripts.generate_connectivity_matrices --skip-conn--igc-conn=$igc_conn
+  python -m scripts.generate_connectivity_matrices --skip-conn --igc-conn=$igc_conn
 done
