ab1d4c1d2f62880ba6581370593a42444e3f4443
Marlon <marlonvcendron@gmail.com>
2025-07-14 23:33:27 -0300

    avg activity

--- scripts/data/avg_activity_broken.py
+++ scripts/data/avg_activity_broken.py
@@ -20,12 +20,12 @@ plt.rcParams.update({
     # "text.usetex": True,
     "font.family": "serif",
     "font.serif": ["Times New Roman"],
-    "font.size": 16,
-    "axes.titlesize": 23,
-    "axes.labelsize": 22,
-    "xtick.labelsize": 16,
-    "ytick.labelsize": 16,
-    "legend.fontsize": 20,
+    # "font.size": 16,
+    # "axes.titlesize": 23,
+    # "axes.labelsize": 22,
+    # "xtick.labelsize": 16,
+    # "ytick.labelsize": 16,
+    # "legend.fontsize": 20,
 
     "lines.linewidth": 5,
     'lines.solid_joinstyle': 'round',
@@ -105,16 +105,21 @@ def in_similarity():
     std_errors_i[group] = std_error_iad
     std_errors_m[group] = std_error_mad
 
-  fig, ax = plt.subplots(figsize=(10, 10), dpi=300)
+  # fig, ax = plt.subplots(figsize=(10, 10), dpi=300)
+  fig, (ax, ax2) = plt.subplots(2, 1, sharex=True, gridspec_kw={'height_ratios': [1, 1]}, figsize=(6, 6), dpi=300)
+  fig.subplots_adjust(hspace=0.05)
+
+
   # fig, ax = plt.subplots()
 
   c_color = "#d64e12"
   cmap = LinearSegmentedColormap.from_list('neuro_cmap', ["#16a4d8", '#8bd346'])
 
   # ax.yaxis.set_major_formatter(PercentFormatter(xmax=1))
-  ax.yaxis.set_major_locator(MaxNLocator(nbins=15))
   formatter = FuncFormatter(lambda y, _: f'{y*100:.0f}')
-  ax.yaxis.set_major_formatter(formatter)
+  for axis in [ax, ax2]:
+    axis.yaxis.set_major_locator(MaxNLocator(nbins=5))
+    axis.yaxis.set_major_formatter(formatter)
 
 
   ads = [ads[group] for group in groups]
@@ -125,50 +130,95 @@ def in_similarity():
   std_errors_i = [std_errors_i[group] for group in groups]
   std_errors_m = [std_errors_m[group] for group in groups]
 
-  # ax.axhline(y=ads[0], color=c_color, linestyle='--')
-  ax.axhline(y=ads[0], color=c_color, linestyle='--')
+  for axis in [ax, ax2]:
+    axis.axhline(y=ads[0], color=c_color, linestyle='--')
 
   ng_groups = groups[1:]
   
   alpha = 0.8
-  ax.plot(ng_groups, ads[1:], color='#8bd346', label='Full GC population pattern', marker='', alpha=alpha)
-  ax.plot(ng_groups, iads[1:], color='#16a4d8', label='iGC pattern', marker='', alpha=alpha)
-  ax.plot(ng_groups, mads[1:], color='#9b5fe0', label='mGC pattern', marker='', alpha=alpha)
 
-  _,_,barlinecols = ax.errorbar(
-      ng_groups,
-      ads[1:],
-      yerr=std_errors[1:],
-      ecolor='#8bd346',
-      linestyle='None'
-  )
-  plt.setp(barlinecols[0], capstyle="round")
+  for axis in [ax, ax2]:
+    axis.plot(ng_groups, ads[1:], color='#8bd346', label='Padrão população GC', marker='', alpha=alpha)
+    axis.plot(ng_groups, iads[1:], color='#16a4d8', label='Padrão iGC', marker='', alpha=alpha)
+    axis.plot(ng_groups, mads[1:], color='#9b5fe0', label='Padrão mGC', marker='', alpha=alpha)
 
-  _,_,barlinecols = ax.errorbar(
-      ng_groups,
-      iads[1:],
-      yerr=std_errors_i[1:],
-      ecolor='#16a4d8',
-      linestyle='None'
-  )
-  plt.setp(barlinecols[0], capstyle="round")
-  
-  _,_,barlinecols = ax.errorbar(
-      ng_groups,
-      mads[1:],
-      yerr=std_errors_m[1:],
-      ecolor='#9b5fe0',
-      linestyle='None'
-  )
-  plt.setp(barlinecols[0], capstyle="round")
+    _,_,barlinecols = axis.errorbar(
+        ng_groups,
+        ads[1:],
+        yerr=std_errors[1:],
+        ecolor='#8bd346',
+        linestyle='None'
+    )
+    plt.setp(barlinecols[0], capstyle="round")
+
+    _,_,barlinecols = axis.errorbar(
+        ng_groups,
+        iads[1:],
+        yerr=std_errors_i[1:],
+        ecolor='#16a4d8',
+        linestyle='None'
+    )
+    plt.setp(barlinecols[0], capstyle="round")
+    
+    _,_,barlinecols = axis.errorbar(
+        ng_groups,
+        mads[1:],
+        yerr=std_errors_m[1:],
+        ecolor='#9b5fe0',
+        linestyle='None'
+    )
+    plt.setp(barlinecols[0], capstyle="round")
       
+  all_ys = np.concatenate([ads[1:], iads[1:], mads[1:]])
+  y_with_error_min = np.concatenate([
+      np.array(ads[1:]) - np.array(std_errors[1:]),
+      np.array(iads[1:]) - np.array(std_errors_i[1:]),
+      np.array(mads[1:]) - np.array(std_errors_m[1:])
+  ])
+  y_with_error_max = np.concatenate([
+      np.array(ads[1:]) + np.array(std_errors[1:]),
+      np.array(iads[1:]) + np.array(std_errors_i[1:]),
+      np.array(mads[1:]) + np.array(std_errors_m[1:])
+  ])
+  
+  min_val = np.min(y_with_error_min)
+  max_val = np.max(y_with_error_max)
+
+  ax.set_ylim(.17, max_val * 1.05)
+  ax2.set_ylim(min_val * .95, .17)
+
+  ax.spines['bottom'].set_visible(False)
+  ax2.spines['top'].set_visible(False)
+  ax.xaxis.tick_top()
+  ax.tick_params(labeltop=False)
+  ax2.xaxis.tick_bottom()
+
+  d = .015
+  kwargs = dict(transform=ax.transAxes, color='k', clip_on=False)
+  ax.plot((-d, +d), (-d, +d), **kwargs)
+  ax.plot((1 - d, 1 + d), (-d, +d), **kwargs)
+
+  kwargs.update(transform=ax2.transAxes)
+  ax2.plot((-d, +d), (1 - d, 1 + d), **kwargs)
+  ax2.plot((1 - d, 1 + d), (1 - d, 1 + d), **kwargs)
+
+  # for i, group in enumerate(ng_groups):
+  #   t_stat, p_value = ttest_ind(
+  #       group_ads['control_ca3'],
+  #       group_ads[group],
+  #       equal_var=False  # Welch's t-test (doesn't assume equal variance)
+  #   )
+
+  #   if p_value < 0.001:
+  #     y_value = ads[i+1] + std_errors[i+1]
+  #     ax.text(i, y_value + 0.002, '*', ha='center', va='bottom', color='black', fontsize=20, fontweight='bold')
 
   ax.legend(frameon=False)
 
   sm = plt.cm.ScalarMappable(cmap=cmap, norm=plt.Normalize(vmin=0.1, vmax=1.0))
   sm.set_array([])
 
-  plt.ylabel('Average population activation degree $\\mathcal{A}_D$ (%)')
+  fig.supylabel('Average population activation degree $\\mathcal{A}_D$ (%)')
   plt.xlabel('Neurogenesis models with X% connectivity fraction')
 
   xlabels = range(10, 101, 10)
@@ -179,21 +229,6 @@ def in_similarity():
   # plt.show()
   plt.savefig(f'figures/plots/avg_activity.jpg', dpi=300, format='jpg')
   plt.close()
-
-  # print(f'full: min: {np.min(ads[1:])}, max: {np.max(ads[1:])}')
-  # print(f'iGCs: min: {np.min(iads[1:])}, max: {np.max(iads[1:])}')
-  # print(f'mGCs: min: {np.min(mads[1:])}, max: {np.max(mads[1:])}')
-
-  # for group in groups:
-  #   if (group == 'control'):
-  #     continue
-
-  #   t_stat, p_value = ttest_ind(
-  #       group_ads['control'],
-  #       group_ads[group],
-  #       equal_var=False  # Welch's t-test (doesn't assume equal variance)
-  #   )
-  #   print(f"t-statistic for {group}: {t_stat}, p-value: {p_value} {'**' if p_value < 0.05 else ''}")
 
 
 in_similarity()
