38e29b2f66f78a2471a9f8ff78b259f118c82d44
Marlon <marlonvcendron@gmail.com>
2025-07-24 09:42:41 -0300

    diagrama

--- thesis/scripts/build.sh
+++ thesis/scripts/build.sh
@@ -5,3 +5,4 @@
 latexmk -pdf -output-directory=build project.tex -f
 
 cp build/project.pdf ./
+cp build/project.pdf ./Projeto\ -\ Modelo\ computacional\ sobre\ a\ dinâmica\ temporal\ da\ neurogênese\ adulta\ no\ giro\ denteado\ e\ seu\ impacto\ nas\ funções\ de\ memória\ do\ CA3.pdf 
