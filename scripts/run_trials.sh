1641f982813c47e25c4769f109401da1b3d4984e
Marlon <marlonvcendron@gmail.com>
2026-03-31 20:32:26 -0300

    improve run trials

--- scripts/run_trials.sh
+++ scripts/run_trials.sh
@@ -2,6 +2,7 @@
 
 prefix=$1
 trials=30
+seq=$(seq 0.1 0.1 1.0)
 
 if [ -z "$prefix" ]; then
   echo "Usage: $0 <prefix>"
@@ -14,10 +15,18 @@ else
   trials=$2
 fi
 
+if [ -z "$3" ]; then
+  echo "No seq specified, using default: $(echo $seq | tr '\n' ' ')"
+else
+  seq=$3
+fi
+
+echo "Running $prefix for $trials trials with igc seq $seq"
+
 echo "Running: control"
 python -m main --trials $trials --prefix $prefix --no-neurogenesis
 
-for igc_conn in $(seq 0.1 0.1 1.0); do
+for igc_conn in $seq; do
   echo "Running: neurogenesis --igc-conn $igc_conn"
   python -m main --trials $trials --igc-conn=$igc_conn --prefix $prefix
 done
