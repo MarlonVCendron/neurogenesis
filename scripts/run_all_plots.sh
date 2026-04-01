38ce13d35eadfc7dbdbdd1b1d7af4627b6698977
Marlon <marlonvcendron@gmail.com>
2026-03-31 21:00:10 -0300

    run all plots

---
+++ scripts/run_all_plots.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+python -m scripts.data.avg_activity_broken
+python -m scripts.data.avg_activity_ca3
+# python -m scripts.data.avg_activity
+python -m scripts.data.avg_pattern_integration
+python -m scripts.data.avg_pattern_separation
+python -m scripts.data.firing_rate_distribution
+python -m scripts.data.pattern_integration
+python -m scripts.data.pattern_separation
+python -m scripts.data.sparsity
