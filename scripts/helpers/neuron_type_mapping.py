cc0fe17b65a8f74573362bd5a23064b489f9c7aa
Marlon <marlonvcendron@gmail.com>
2026-07-04 10:50:25 -0300

    abbreviations

--- scripts/helpers/neuron_type_mapping.py
+++ scripts/helpers/neuron_type_mapping.py
@@ -8,3 +8,19 @@ neuron_type_mapping = {
     "pca3": "CA3 Pyramidal",
     "ica3": "CA3 Inhibitory",
 }
+
+neuron_type_abbreviation = {
+    "pp": "EC",
+    "mgc": "mGC",
+    "igc": "iGC",
+    "mc": "MC",
+    "hipp": "HIPP",
+    "bc": "BC",
+    "pca3": "pCA3",
+    "ica3": "iCA3",
+}
+
+
+def display_label(key, use_abbreviation=True):
+    table = neuron_type_abbreviation if use_abbreviation else neuron_type_mapping
+    return table.get(key, key)
