c8667d6f6a0c4819c0ecfd03298ea97d9e4ff07c
Marlon <marlonvcendron@gmail.com>
2026-08-02 22:06:30 -0300

    bib

--- paper/scripts/build.sh
+++ paper/scripts/build.sh
@@ -6,7 +6,7 @@ BSTINPUTS=./bst: latexmk -pdf -pdflatex="pdflatex" -output-directory=build sn-ar
 
 mv build/sn-article.pdf ./
 
-latexmk -pdf -pdflatex="pdflatex" -output-directory=build online-resource-1.tex -f
+BSTINPUTS=./bst: latexmk -pdf -pdflatex="pdflatex" -output-directory=build online-resource-1.tex -f
 
 mv build/online-resource-1.pdf ./
 
